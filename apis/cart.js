ca8d12773ad93f41b4d4317da09bc569b2ccc84d
Marine <jeanningros.marine@gmail.com>
2020-12-10 17:10:15 +0100

    displaying cart

--- apis/cart.js
+++ apis/cart.js
@@ -3,3 +3,32 @@ let selectedLense = localStorage.getItem('selectedLense')
 
 console.log(cameraId, selectedLense);
 //FIXME not getting the right lense option, always the first one comes up (value=0).
+
+
+camerasUrl = `http://localhost:3000/api/cameras/${cameraId}`;
+
+let cameras;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", camerasUrl, true);
+
+  xhr.onload = function (){
+    // console.log(this.status);
+    //FIXME ne pas oublier les promises au lieu des callbacks
+    if (this.status == 200) {
+      var cameras = JSON.parse(this.responseText);
+
+      selectedCam.textContent = cameras.name;
+      console.log(selectedCam.textContent);
+
+      camLense.textContent = `${cameras.lenses[selectedLense]}`;
+      console.log(selectedLense);
+
+      camPrice.textContent = cameras.price+ "€";
+      console.log(camPrice.textContent);
+
+    }
+
+  };
+
+  xhr.send();
