7e6ab587c52ead3a208b42617c839f999f3bb8bb
Marine <jeanningros.marine@gmail.com>
2021-01-13 20:42:25 +0100

    POST réussi

--- frontend/order.js
+++ frontend/order.js
@@ -12,45 +12,23 @@ console.log(finalObj);
 
 camerasUrl = `http://localhost:3000/api/cameras/order`;
 
-/* const contactString = JSON.stringify(contactObj);
-const cameraString = JSON.stringify(cameraArr); */
+var xhr = new XMLHttpRequest();
 
-//var xhr = new XMLHttpRequest();
-//xhr.open("POST", camerasUrl, true);
+xhr.open("POST", camerasUrl, true);
 
-/* xhr.onload = function () {
-  // console.log(this.status);
-  if (this.status == 200) {
+xhr.onload = function () {
+  
+  if (this.status == 201) {
     var order = JSON.parse(this.responseText);
     console.log(order);
+    console.log(this.status);
+  } else {
+    console.log(this.status);
   }
-}; */
-
-//xhr.setRequestHeader("Content-type", "application/json");
-//xhr.send(contactObjStr, cameraArrStr);
-
-var xhr = new XMLHttpRequest();
-xhr.open("POST", camerasUrl, true);
-// set `Content-Type` header
-xhr.setRequestHeader('Content-Type', 'application/json');
-// send request with JSON payload (test data)
- let json = {
-   contact: {
-    firstName: "John",
-    lastName: "Doe",
-    address: "random address",
-    city: "Vancouver",
-    email: "test@example.com"
-   },
-   products: [1, 2]
- };
+};
 
 xhr.setRequestHeader("Content-type", "application/json");
-//xhr.send(JSON.stringify(finalObj));
-xhr.send(JSON.stringify(json));
+xhr.send(JSON.stringify(finalObj));
 
-console.log(xhr.readyState)
-
-//FIXME look at camera.js
 
 
